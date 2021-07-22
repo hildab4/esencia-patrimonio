cfab232584a59d6309d6e90bdb443cfd9cae8b5e
hildab4 <83654283+hildab4@users.noreply.github.com>
2021-07-22 11:28:48 -0500

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,241 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#aad8a4",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View website",
+    popups: "hover",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    MEX2706: {
+      name: "Baja California",
+      popups: "https://www.sonora.gob.mx",
+      url: "https://drive.google.com/file/d/1Iu_y0kl9XshlKEtWH7GjAvrRre-fCKcM/view?usp=sharing",
+      image_url: "https://media-cdn.tripadvisor.com/media/photo-s/1c/7d/b8/c7/la-paz-mexico-photo-by.jpg"
+    },
+    MEX2707: {
+      name: "Baja California Sur",
+      url: "https://drive.google.com/file/d/148VQF5GT-VXkb4saA6bQTJhaPuQlbBxk/view?usp=sharing",
+      image_url: "https://funlifecrisis.com/wp-content/uploads/2018/07/15-top-things-to-do-in-baja-california.jpg"
+    },
+    MEX2708: {
+      name: "Coahuila",
+      url: "https://drive.google.com/file/d/1TDQvxoobHhRecne7skuvxUMeqOxPOys0/view?usp=sharing",
+      image_url: "https://cdn.mexicodestinos.com/lugares/parque-nacional-los-novillos-coahuila-galeria-min.jpg"
+    },
+    MEX2709: {
+      name: "Chihuahua",
+      url: "https://drive.google.com/file/d/1IXKSCD_zOfRIIyPmOa9hZFlZNbue7CLu/view?usp=sharing",
+      image_url: "https://i.pinimg.com/originals/0c/dc/93/0cdc935c1bad210125365a1d5b10e0d9.jpg"
+    },
+    MEX2710: {
+      name: "Durango",
+      url: "https://drive.google.com/file/d/1jHWZLCuo7UzQJcBxW4cc7s9RClhp_nOW/view?usp=sharing",
+      image_url: "https://www.gob.mx/cms/uploads/article/main_image/36534/Plaza_Armas_Durango.jpg"
+    },
+    MEX2711: {
+      name: "Sinaloa",
+      url: "https://drive.google.com/file/d/1n3bQZ-qNYkKROTdqWDsxdv-wg7Zqcd2u/view?usp=sharing",
+      image_url: "https://newsweekespanol.com/wp-content/uploads/2019/04/Group-546-e1556065094111.jpg"
+    },
+    MEX2712: {
+      name: "Sonora",
+      url: "https://drive.google.com/file/d/12_KqKh0BU9n0z0yZT24ddlnDXhl6CMth/view?usp=sharing",
+      image_url: "https://pbs.twimg.com/media/D3jnpXZWkAAusyU.jpg"
+    },
+    MEX2713: {
+      name: "Zacatecas",
+      url: "https://drive.google.com/file/d/16w1S26m8a3NO88denxtZHR9b6Bd4H7h4/view?usp=sharing",
+      image_url: "https://inmobiliare.com/himalaya/wp-content/uploads/2020/10/zacatecas_turismo_1-1024x684.jpg"
+    },
+    MEX2714: {
+      name: "Nuevo León",
+      url: "https://drive.google.com/file/d/1dgnI5QMBLfTfDN7D-iMRHAXLbwXHtTq4/view?usp=sharing",
+      image_url: "https://www.gob.mx/cms/uploads/article/main_image/80669/nuevo-leon-monterrey-mexico.jpg"
+    },
+    MEX2715: {
+      name: "San Luis Potosí",
+      url: "https://drive.google.com/file/d/1f_5nn47jzk4WRditTUpbw0F3gaqiWGSq/view?usp=sharing",
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Catedral_SLP_cielo.jpg/1200px-Catedral_SLP_cielo.jpg"
+    },
+    MEX2716: {
+      name: "Tamaulipas",
+      url: "https://drive.google.com/file/d/1_Ty0xRAHGeeSxmILzvnzKaB5jzfJiX3X/view?usp=sharing",
+      image_url: "https://www.visit-mexico.mx/wp-content/uploads/2021/01/Tamaulipas-Cover-01.jpg"
+    },
+    MEX2717: {
+      name: "Aguascalientes",
+      url: "https://drive.google.com/file/d/1C2yJh7JvSDVxALui4ZmR6aYfGjPIL6SR/view?usp=sharing",
+      image_url: "https://www.eluniversal.com.mx/sites/default/files/N-A15-EU280920-4_DrupalMainImagenVertical.var_1601268187.jpg"
+    },
+    MEX2718: {
+      name: "Colima",
+      url: "https://drive.google.com/file/d/1evH2E6i2V8AXvGwSZ_DHQccztzPVUGJS/view?usp=sharing",
+      image_url: "https://www.mexicodesconocido.com.mx/wp-content/uploads/2018/09/colima.jpg"
+    },
+    MEX2719: {
+      name: "Jalisco",
+      url: "https://drive.google.com/file/d/1wr1ApM6aLD7JuHrZ1cp35NjkSQVXD5nW/view?usp=sharing",
+      image_url: "https://topadventure.com/__export/1622162179676/sites/laverdad/img/2021/05/27/5_pueblos_imperdibles_de_los_altos_de_jalisco.jpg_1538342946.jpg"
+    },
+    MEX2720: {
+      name: "Michoacán",
+      url: "https://drive.google.com/file/d/1z-XeMcAMt2_N1RTi6QRXMWYxfs1IIUlj/view?usp=sharing",
+      image_url: "https://malvatraveltomexico.com.mx/wp-content/uploads/2020/05/0-Portada.jpg"
+    },
+    MEX2721: {
+      name: "Nayarit",
+      url: "https://drive.google.com/file/d/18CN1037N-eZkDAHt6y6rf67TIAD1ZFh_/view?usp=sharing",
+      image_url: "https://inmobiliare.com/himalaya/wp-content/uploads/2020/02/riviera-nayarit-one-only-mandarina-hoteles-de-lujo-turismo_2.jpg"
+    },
+    MEX2722: {
+      name: "Campeche",
+      url: "https://drive.google.com/file/d/17Qeb2zRcuJycHxs_Ij6-_RQwJ5v53pgw/view?usp=sharing",
+      image_url: "https://www.hola.com/imagenes/viajes/20180301121047/campeche-top-buena-vida-mexico/0-545-766/campeche-plaza-catedral-t.jpg"
+    },
+    MEX2723: {
+      name: "Oaxaca",
+      url: "https://drive.google.com/file/d/1rliKvoCap3sriSUx_tHRidgHGttueiXw/view?usp=sharing",
+      image_url: "https://media.nomadicmatt.com/2021/oaxaca1.jpg"
+    },
+    MEX2724: {
+      name: "Puebla",
+      url: "https://drive.google.com/file/d/1MZtucX2XJMawinDclHOpzj4eLOsY987e/view?usp=sharing",
+      image_url: "https://res.cloudinary.com/dsmafmqwi/image/upload/c_fill,f_auto,h_1280,q_auto/v1/virtualtrips/locations/jwk8lnk5lounn1ojfxap"
+    },
+    MEX2725: {
+      name: "Tabasco",
+      url: "https://drive.google.com/file/d/1mcgXl-fEziSD_VNCXvjKz8hEi48nbsec/view?usp=sharing",
+      image_url: "https://www.elheraldodetabasco.com.mx/local/iy4ik7-carlos.jpg/ALTERNATES/LANDSCAPE_400/carlos.jpg"
+    },
+    MEX2726: {
+      name: "Tlaxcala",
+      url: "https://drive.google.com/file/d/14C05wmKUTPpLaYvZEIjh86r42tIat-CT/view?usp=sharing",
+      image_url: "https://promoturqueretaro.com.mx/wp-content/uploads/2019/06/Viajes-cultural-a-Tlaxcala.jpg"
+    },
+    MEX2727: {
+      name: "Distrito Federal",
+      url: "https://drive.google.com/file/d/1Bd6HllAQd68hZ8dUF2vHJiZI9GcXnG0t/view?usp=sharing",
+      image_url: "https://lonelyplanetes.cdnstatics2.com/sites/default/files/styles/max_1300x1300/public/fotos/mexico_ciudaddemexico_paseoreforma_angelindependencia_shutterstockrf_1066463744_aberu.go_shutterstock.jpg?itok=MjJFsqoK"
+    },
+    MEX2728: {
+      name: "Guanajuato",
+      url: "https://drive.google.com/file/d/17d83Py8ZzXXcgGcFd5NPspcRJldrbk_u/view?usp=sharing",
+      image_url: "https://cdn.britannica.com/00/188200-050-1995DFEE/view-city-Guanajuato-foreground-Mexico-basilica.jpg"
+    },
+    MEX2729: {
+      name: "Guerrero",
+      url: "https://drive.google.com/file/d/1hExlXrJwHzgXSprGHPG6CE02HTqn0fQQ/view?usp=sharing",
+      image_url: "https://realestatemarket.com.mx/images/2020/01-Enero/0701/guerrero_turismo_noticias_nacional_.jpg"
+    },
+    MEX2730: {
+      name: "Hidalgo",
+      url: "https://drive.google.com/file/d/1PdO1Mfw7zh5lKBe8A_7DtGI5W0MWq9-i/view?usp=sharing",
+      image_url: "https://ychef.files.bbci.co.uk/live/624x351/p067619b.jpg"
+    },
+    MEX2731: {
+      name: "México",
+      url: "https://drive.google.com/file/d/1UqPQfhCHrBxZFSN5DUpLqkdIY_NftM9K/view?usp=sharing",
+      image_url: "https://www.unionedomex.mx/sites/default/files/styles/galeria/public/field/image/edomex.jpg?itok=e-Eg1wbv"
+    },
+    MEX2732: {
+      name: "Morelos",
+      url: "https://drive.google.com/file/d/19sqEfhCZxgm57e8qAvOkuHkR-T0lKkAm/view?usp=sharing",
+      image_url: "https://img.rezdy.com/PRODUCT_IMAGE/54415/Cuernavaca_Mexico_lg.jpg"
+    },
+    MEX2733: {
+      name: "Querétaro",
+      url: "https://drive.google.com/file/d/1LtiZm14zaBcoFgssqvck18IDs40ZCAx2/view?usp=sharing",
+      image_url: "http://www.conexionmexico.com.mx/wp-content/uploads/2018/04/Vivir-en-QRO.jpg"
+    },
+    MEX2734: {
+      name: "Veracruz",
+      url: "https://drive.google.com/file/d/1rMq6QsvtPj_AdQOmQp_hyG2tvZOwHO4T/view?usp=sharing",
+      image_url: "https://palabrasclaras.mx/wp-content/uploads/2019/06/zocalo-veracruz-1068x720.jpg"
+    },
+    MEX2735: {
+      name: "Chiapas",
+      url: "https://drive.google.com/file/d/1EdqHJhixWd9ZmQqavLmNZMAQiDvhy2XF/view?usp=sharing",
+      image_url: "https://res.cloudinary.com/worldpackers/image/upload/c_fill,f_auto,q_auto,w_1024/v1/guides/article_cover/qjrpk8y82hjmprbjmdgv"
+    },
+    MEX2736: {
+      name: "Quintana Roo",
+      url: "https://drive.google.com/file/d/1Rm14QunJtg9jWvTiJ_jye8aymY2DrHwA/view?usp=sharing",
+      image_url: "https://cdn-3.expansion.mx/f9/99/a80537104c4a82b0a94d93e398d2/cancun-quintana-roo.jpg"
+    },
+    MEX2737: {
+      name: "Yucatán",
+      url: "https://drive.google.com/file/d/1Mh9IIf97Prp0t5NiBNEHNz4tENcDb9P5/view?usp=sharing",
+      image_url: "https://www.barcelo.com/pinandtravel/wp-content/uploads/2019/10/que-ver-peninsula-yucatan-1024x466.jpg"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
